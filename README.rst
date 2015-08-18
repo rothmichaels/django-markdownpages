7ff012a6c54099a3a7d81fed43cc89ef41a89df0
Roth Michaels <roth@rothmichaels.us>
2015-08-17 22:01:19 -0400

    Ready to test packaging.

---
+++ README.rst
@@ -0,0 +1,31 @@
+Markdown Pages
+==============
+
+Markdown pages is a simple Django app to render a directory of Markdown
+files as HTML.
+
+Quick Start
+-----------
+
+1. Add "markdownpages" to your INSTALLED_APPS setting like this::
+
+     INSTALLED_APPS = (
+         ...
+         'markdownpages',
+     )
+
+2. Add path to your markdown file directory to settings.py::
+
+     MARKDOWN_PAGES_ROOT = '/path/to/your/markdown/directory'
+
+3. Include the Markdown Pages URLConf in your project urls.py by adding
+   the folowing as the final url in urlpatterns:
+
+    url(r'^', include('markdownpages.urls'))
+
+4. Start server!
+
+License
+-------
+Copyright (c) 2015 Roth Michaels. Distributed under the Eclipse Public License v1.0.
+
---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-markdownpages',
+    version='0.1',
+    packages=['markdownpages'],
+    include_package_data=True,
+    license='Eclipse Public License v1.0',
+    description='A simple Django app to conduct Web-based polls.',
+    long_description=README,
+    url='http://github.com/rothmichaels/django-markdownpages',
+    author='Roth Michaels',
+    author_email='roth@rothmichaels.us',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
