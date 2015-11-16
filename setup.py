a20f632958ebea6d07d94629d7298957a186784f
Roth Michaels <roth@rothmichaels.us>
2015-11-15 19:46:29 -0500

    v0.1.2 Fixed setup.py description

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-markdownpages',
-    version='0.1.1',
+    version='0.1.2',
     packages=['markdownpages'],
     install_requires=['markdown'],
     include_package_data=True,
     license='Eclipse Public License v1.0',
-    description='A simple Django app to conduct Web-based polls.',
+    description='Django app providing a view to server a tree of rendered markdown files.',
     long_description=README,
     url='http://github.com/rothmichaels/django-markdownpages',
     author='Roth Michaels',
